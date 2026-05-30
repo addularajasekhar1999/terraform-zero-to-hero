8f19ba0abf5eb2a14dcd920a9e88ddf562e28a9d
addularajasekhar1999 <68265794+addularajasekhar1999@users.noreply.github.com>
2026-05-31 00:12:34 +0530

    Comment out S3 backend configuration
    Comment out the terraform backend configuration for S3.

--- Day-1/PROJECT-ec2-instance-creation/main.tf
+++ Day-1/PROJECT-ec2-instance-creation/main.tf
@@ -2,13 +2,13 @@ provider "aws" {
     region = "eu-west-1"  # Set your desired AWS region
 }
 
-terraform {
-  backend "s3" {
-    bucket         = "puliars" # change this
-    key            = "aws.tfstate"
-    region         = "eu-west-1"
-  }
-}
+#terraform {
+#  backend "s3" {
+#    bucket         = "puliars" # change this
+#    key            = "aws.tfstate"
+#    region         = "eu-west-1"
+#  }
+#}
 
 resource "aws_instance" "example" {
     ami           = "ami-0de864d6a3bd20ea8"  # Specify an appropriate AMI ID
